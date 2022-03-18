9c0d63e52e59cd1a4b9c74659524ce7fd6549df3
andy sarbini <kangandy09@gmail.com>
2022-03-18 21:51:42 +0700

    part 7 - shopping cart
    https://www.youtube.com/watch?v=pQf_zj7yKYM&list=PLz_YkiqIHesvPtvLl2Wz5FtuW44dBt199&index=8

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Gloudemans\Shoppingcart\ShoppingcartServiceProvider::class,
         
         /*
         * Package Service Providers...
@@ -234,6 +235,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+        'Cart' => \Gloudemans\Shoppingcart\Facades\Cart::class,
 
     ],
 
